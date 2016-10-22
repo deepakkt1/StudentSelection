5126e78a5ec54e3a30dfe9866e39a88ea76390d0
hittecd <hittecd@gmail.com>
2016-10-21 19:19:39 -0600

    fixed database config issue

--- dlap_web_app/dlap_web_app/settings.py
+++ dlap_web_app/dlap_web_app/settings.py
@@ -82,7 +82,6 @@ WSGI_APPLICATION = 'dlap_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': BASE_DIR + '/loginapp.sqlite',  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
@@ -90,7 +89,7 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',   # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',   # Set to empty string for default.
-    }    }
+    } 
 }
 
 AUTH_PASSWORD_VALIDATORS = [
