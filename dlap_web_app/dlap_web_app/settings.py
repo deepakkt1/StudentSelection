c3f2e710f3d7dc6aa4f54d685ded666f5419d362
hittecd <hittecd@gmail.com>
2016-10-21 18:50:26 -0600

    merging tutorial code into main project

--- dlap_web_app/dlap_web_app/settings.py
+++ dlap_web_app/dlap_web_app/settings.py
@@ -81,9 +81,15 @@ WSGI_APPLICATION = 'dlap_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': BASE_DIR + '/loginapp.sqlite',  # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',   # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',   # Set to empty string for default.
+    }    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
--- dlap_web_app/dlap_web_app/urls.py
+++ dlap_web_app/dlap_web_app/urls.py
@@ -17,6 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home),
     url(r'^submit/', include('submit.urls')),
     url(r'^admin/', admin.site.urls),
 ]
