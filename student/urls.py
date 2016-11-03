e16a5e4e50af44b523853f18a9f3a94041fc7496
deepakkt1 <deky6365@colorado.edu>
2016-11-03 06:00:20 -0600

    student form update

--- student/urls.py
+++ student/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^student.html$', views.student, name='student'),
     url(r'^$', views.studinfo, name='studentsubmit'),
+    url(r'^student.html$', views.student, name='student'),
 ]
 
